cdb4f70e71cff62e2ca2f8ed9a1da9985467b53d
Rachel Cheyfitz <spinrach@gmail.com>
2025-09-25 15:16:48 +0300

    Add Cloudflare config and update Next.js settings
    Introduces next.config.ts and open-next.config.ts for enhanced configuration and Cloudflare integration. Updates Next.js settings to use a '/manufacturing' base path and asset prefix, and adds wrangler.json/c for Cloudflare deployment. Also updates dependencies in package-lock.json and modifies webflow.json.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/manufacturing',
+  assetPrefix: '/manufacturing',
   output: 'export',
   trailingSlash: true,
   images: {
---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from "next";
+import userConfig from './clouduser.next.config';
+
+const webflowOverrides: NextConfig = {
+  basePath: '/manufacturing',
+  assetPrefix: 'https://20faf3fb-6591-4e16-8393-a1f01e39f7f7.wf-app-prod.cosmic.webflow.services/manufacturing',
+  images: {
+    ...userConfig.images,
+    // TODO: determine whether any of the non-custom loader options (imgix, cloudinary, akamai) work
+    // and if so allow them to be used here
+    loader: 'custom',
+    loaderFile: userConfig.images?.loaderFile || './webflow-loader.ts',
+  },
+};
+
+const nextConfig: NextConfig = {
+  ...userConfig,
+  ...webflowOverrides,
+};
+
+export default nextConfig;
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
